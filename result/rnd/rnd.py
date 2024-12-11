d5da2c146ae5b2c60519ba272dbcb87921dab991
SS3M <buisam6565@gmail.com>
2024-12-11 10:00:51 +0700

    Add random net for easy testing

---
+++ result/rnd/rnd.py
@@ -0,0 +1,12 @@
+import torch.nn as nn
+import torch
+
+class RandomQNetwork(nn.Module):
+    def __init__(self, observation_shape, action_shape, device='cpu'):
+        super().__init__()
+        self.observation_shape = observation_shape
+        self.action_shape = action_shape
+        self.device = device
+
+    def forward(self, obs):
+        return torch.rand((1, self.action_shape))
